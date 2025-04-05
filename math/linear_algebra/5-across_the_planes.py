5b6abd83990aac46124ab4d8d1d85e28fdeda01d
Huang1810 <ahhamed1810@gmail.com>
2025-04-05 13:13:43 +0100

    commit

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to add two 2D matrices element-wise.
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2D matrices element-wise.
+
+    Args:
+        mat1 (list of list of int/float): First matrix.
+        mat2 (list of list of int/float): Second matrix.
+
+    Returns:
+        list or None: New matrix with element-wise sums or None
+                      if shapes differ.
+    """
+    if len(mat1) != len(mat2) or any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
+        return None
+    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
