e8d3f368e46b16a1892eb8934b1ef9785ee07334
Huang1810 <ahhamed1810@gmail.com>
2025-07-26 20:00:41 +0100

    commit

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
-"""PCA function"""
-
+"""
+PCA on a dataset
+"""
 import numpy as np
+
 
 def pca(X, var=0.95):
     """
-    Performs PCA on a dataset while retaining a given fraction of variance.
-
-    Args:
-        X (np.ndarray): shape (n, d), centered dataset (zero mean)
-        var (float): desired fraction of variance to retain (0 < var <= 1)
-
-    Returns:
-        np.ndarray: shape (d, nd), weight matrix with nd principal components
+    Function that performs PCA on a dataset
     """
-    # Perform SVD
-    u, s, vh = np.linalg.svd(X, full_matrices=False)
+    # Calculate the SVD of input data
+    U, S, V = np.linalg.svd(X, full_matrices=False)
 
-    # Compute explained variance ratio
-    explained_variance = (s ** 2) / np.sum(s ** 2)
-    cumulative_variance = np.cumsum(explained_variance)
+    # Calculate the cumulative sum of the variance ratio
+    var_ratio = np.cumsum(S**2) / np.sum(S**2)
 
-    # Number of components to retain desired variance
-    r = np.searchsorted(cumulative_variance, var) + 1
+    # Determine the number of components to keep
+    nb_comp = np.argmax(var_ratio >= var) + 1
 
-    # Return projection matrix (d, r)
-    return vh[:r].T
+    # Select first nb_comp components
+    W = V[:nb_comp + 1].T
+
+    return W
