518dcff34017cefcde9b7a5c94796bc30b6303b1
Huang1810 <ahhamed1810@gmail.com>
2025-08-23 21:39:42 +0100

    commit

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -6,14 +6,14 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, filters, latent_dims):
     """ creates a convolutional autoencoder
 
-        - input_dims is a tuple of integers containing the dimensions of the model input
-        - filters is a list containing the number of filters for each conv layer in the encoder
-        - filters should be reversed for the decoder
-        - latent_dims is a tuple of integers containing the dimensions of the latent space
-        Returns: encoder, decoder, auto
-            - encoder is the encoder model
-            - decoder is the decoder model
-            - auto is the full autoencoder model
+        - input_dims (tuple): Dimensions of the model input (H, W, C).
+        - filters (list): Number of filters for each conv layer in the encoder.
+        - latent_dims (tuple): Dimensions of the latent space representation (H, W, C).
+       
+         Returns: encoder, decoder, auto
+            - encoder (Model): Encoder model.
+            - decoder (Model): Decoder model.
+            - auto (Model): Full autoencoder model.
     """
     input_img = keras.Input(shape=input_dims)
 
